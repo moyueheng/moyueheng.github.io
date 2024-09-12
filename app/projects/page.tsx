b332873c451d5b062eef01fe3952614bdd646efd
vimpas <moyueheng@gamil.com>
2024-09-12 16:47:08 +0800

    feat: ✨ 照片更新

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,13 +1,18 @@
+// 导入项目数据和Card组件
 import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
+// 导入生成页面元数据的函数
 import { genPageMetadata } from 'app/seo'
 
+// 生成页面的元数据
 export const metadata = genPageMetadata({ title: 'Projects' })
 
+// Projects页面组件
 export default function Projects() {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        {/* 页面标题和描述部分 */}
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
@@ -16,8 +21,10 @@ export default function Projects() {
             Showcase your projects with a hero image (16 x 9)
           </p>
         </div>
+        {/* 项目卡片列表部分 */}
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
+            {/* 遍历projectsData数组,为每个项目创建一个Card组件 */}
             {projectsData.map((d) => (
               <Card
                 key={d.title}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
           {/* <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} /> */}
           {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} /> */}
           {/* <SocialIcon kind="x" href={siteMetadata.x} size={6} /> */}
-          <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
+          {/* <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} /> */}
           {/* <SocialIcon kind="threads" href={siteMetadata.threads} size={6} /> */}
           <SocialIcon kind="bilibili" href={siteMetadata.bilibili} size={6} />
         </div>
