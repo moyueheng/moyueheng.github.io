c01df44765dd9261657fe31e3dc4598835c6df08
vimpas <moyueheng@gamil.com>
2024-10-10 20:24:25 +0800

    1

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,11 +9,11 @@ const MAX_DISPLAY = 5
 export default function Home({ posts }) {
   return (
     <>
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
